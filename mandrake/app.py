bdd154c910c88ac6ec0187e8d13862c00f77e97a
Victor M. Ramirez <ramirez.vmanuel@gmail.com>
2020-04-10 17:53:27 -0500

    cleaning Lambdas function

--- mandrake/app.py
+++ mandrake/app.py
@@ -3,27 +3,13 @@ import mandrakeService as mk
 
 
 def lambda_main(event, context):
-    obj=mk.getAllEvents() 
-    print("loaaaaading2...")
-    print(obj)
-    print(type(obj))
-    print(json.dumps(obj))
-    print(type(json.dumps(obj)))
-    
+    obj=mk.getAllEvents()     
     return obj
         
     
 
 
 def write_event(event, context):
-    #obj=mk.getAllEvents() 
-    '''artist=event["artist"]
-    date=event["date"]
-    utc_time=event["utc_time"]
-    '''
-    type(event)
-    type(json.dumps(event))
-    print("INSERTIIIING...")
     mk.putItem(event)
     return event
 
