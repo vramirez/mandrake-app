7b6d7eda3ed9bca499471f9eca24dd88c3b08998
Victor Ramirez <ramirez.vmanuel@gmail.com>
2020-04-17 16:46:04 -0500

    cleaned spaces in main file

--- mandrake/app.py
+++ mandrake/app.py
@@ -5,20 +5,15 @@ import mandrakeService as mk
 def lambda_main(event, context):
     obj=mk.getAllEvents()     
     return obj
-        
-    
-
 
 def write_event(event, context):
     mk.putItem(event)
     return event
 
-
 def defoe(event, context):
     #print(event)
     
     return "ok"
-
 
 def getTodayNewerEvents(event, context):
     obj=mk.getTodayNewer()
